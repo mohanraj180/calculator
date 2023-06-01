4b58b98257b954854b824eb1ac063e50fa09c1d2
Mohanraj <mraj8867@gmail.com>
2023-06-01 07:59:14 +0530

    Fix getOperatorIndex() swith case logic

--- script.js
+++ script.js
@@ -39,10 +39,57 @@ switch (operator) {
         const display = document.querySelector("#display");
         display.textContent += e.target.textContent;
     }
-    
-    let num1, num2, operator;
 
+    function getOperatorIndex(str){
+        switch (true) {
+            case str.includes("+"):
+                return str.indexOf("+")
+        
+            case str.includes("-"):
+                return str.indexOf("-")
+        
+            case str.includes("*"):
+                return str.indexOf("*")
+        
+            case str.includes("/"):
+                return str.indexOf("/")
+        }
+        
+    }
+    
+    function addOperator(e){
+        const display = document.querySelector("#display");
+        display.textContent += e.target.textContent;
+    }
+    
+    let num1 = 0, num2 = 0, operator ="";
+
+    // listen number buttons
     const numbers = document.querySelectorAll(".num");
     numbers.forEach(function(number){
         number.addEventListener("click", (e) => display(e));
+    });
+
+    // listen operators
+    const operatorButtons = document.querySelectorAll(".operator");
+    operatorButtons.forEach(function(operator){
+        operator.addEventListener("click", (e) => addOperator(e))
+    });
+
+    // listen equals
+    const equals = document.querySelector("#equals");
+    equals.addEventListener("click", (e) => {
+        const display = document.querySelector("#display");
+        display.textContent += e.target.textContent;
+        
+        let input = display.textContent;
+        let operatorIndex = getOperatorIndex(input);
+        operator = input.slice(operatorIndex, operatorIndex + 1);
+        console.log(operator);
     })
+
+// let s = "123+456";
+// let opIn = getOperatorIndex(s)
+// console.log(opIn);
+// let op = s.slice(opIn, opIn + 1);
+// console.log(op);
